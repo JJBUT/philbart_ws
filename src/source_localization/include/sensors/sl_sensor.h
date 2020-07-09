b99c19dd14572328b425905adbb80370f7dc3a13
JackBorer <jackborer@utexas.edu>
2020-07-09 09:53:17 -0500

    Clean up notes

--- src/source_localization/include/sensors/sl_sensor.h
+++ src/source_localization/include/sensors/sl_sensor.h
@@ -36,10 +36,6 @@ class SLSensorData
     public: SLSensorData();
     public: ~SLSensorData();
 
-    // Assign values to sensor like "data.sensor = &anemometer;"
-    // where anemometer is "sl::SLSensor anemometer;"
-    // public: SLSensor* sensor; //Eliminate this and let each sensors data class inherit SLSensor from its own sensor class
-
     public: double time_stamp;
 
     // Frame
