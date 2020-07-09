7703a9b3d8d842116d03c16489ad7137f86d1492
JackBorer <jackborer@utexas.edu>
2020-07-09 14:32:23 -0500

    About to fill out constructor

--- src/source_localization/src/source_localization_node.cpp
+++ src/source_localization/src/source_localization_node.cpp
@@ -11,27 +11,50 @@
 #include "std_msgs/String.h"
 
 
-namespace sl{
+// Maybe once we import the header files this error will do away
+// using namespace sl; 
 
 //Pose hypothesis
-
+static const std::string node_name_ = "sl";
 static const std::string anemometer_topic_ = "anemometer/data";
 static const std::string gas_sensor_topic_ = "gas_sensor/data";
 static const std::string pose_topic_ = "odometry/filtered_map";
 
-class SourceLocalizationNode
+class SLNode
 {
     public:
-        SourceLocalizationNode();
-        ~SourceLocalizationNode();
+      SLNode();
+      ~SLNode();
 
-    private:   
+    private:  
+      std::string base_frame_id_;
+      std::string map_frame_id_; 
+
+      ros::subscriber map_sub_;
+      ros::Subscriber map_pose_sub_;
+      ros::Subscriber anemometer_sub_;
+      ros::Subscriber gas_sensor_sub_;
+
+      //No update allowed, it's either receieved or not
+      bool map_received_; 
   
 };
 
-} //END sl:: namespace
+boost::shared_ptr<SLNode> source_localization_node_ptr;
 
-int main()
+int main(int argc, char** argv)
 {
+  ros::init(argc, argv, node_name_);
+  ros::NodeHandle nh;
+  
+  //Run using ROS, may add .bag file functionality later
+  if (1)
+  {
+    ros::spin();
+  }
+
   return(0);
 }
+
+SLNode::SLNode() {}
+
