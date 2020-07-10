5717bc13ffc7d37a32a7fcf709c23870b5e5e950
JackBorer <jackborer@utexas.edu>
2020-07-10 12:14:34 -0500

    First min iteration

---
+++ src/source_localization/include/map/map.h
@@ -0,0 +1,22 @@
+#include "nav_msgs/OccupancyGrid.h"
+
+// Include guard
+#ifndef MAP_H
+#define MAP_H
+
+// Description for a map
+typdef struct
+{
+    const nav_msgs::OccupancyGrid map;
+
+    double width, height;
+
+} map_t
+
+// Destroy a map
+void map_free(map_t *map);
+
+// Load a new map into our map structure
+map_t load_map(const nav_msgs::OccupancyGrid map);
+
+#endif
