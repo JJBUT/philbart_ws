9edc8da316dc8218db3cac7cdcbb426e3fd00108
JackBorer <jackborer@utexas.edu>
2020-07-13 13:20:02 -0500

    Removed boost smrt_ptr to std lib

--- src/source_localization/include/map/map.h
+++ src/source_localization/include/map/map.h
@@ -1,8 +1,10 @@
-#include "nav_msgs/OccupancyGrid.h"
-
 // Include guard
 #ifndef MAP_H
 #define MAP_H
+
+
+#include "nav_msgs/OccupancyGrid.h"
+#include "memory"
 
 // Structure used to store maps for the pf
 typedef struct
@@ -16,6 +18,6 @@ typedef struct
 } map_t;
 
 // Load a new map into our map structure
-boost::shared_ptr<map_t>  load_map_t(const nav_msgs::OccupancyGrid& map);
+std::shared_ptr<map_t>  load_map_t(const nav_msgs::OccupancyGrid& map);
 
 #endif
--- src/source_localization/src/map/map.cpp
+++ src/source_localization/src/map/map.cpp
@@ -1,9 +1,9 @@
 #include "../include/map/map.h"
 
 // Load map into map_t struct; all it really does is calculate the with and height in meters
-boost::shared_ptr<map_t> load_map_t(const nav_msgs::OccupancyGrid& data)
+std::shared_ptr<map_t> load_map_t(const nav_msgs::OccupancyGrid& data)
 {
-    boost::shared_ptr<map_t> map(new map_t());
+    std::shared_ptr<map_t> map(new map_t());
 
     // Map width (x-dimension) in meters: m/cell * cell
     map->width = data.info.resolution * data.info.width; 
