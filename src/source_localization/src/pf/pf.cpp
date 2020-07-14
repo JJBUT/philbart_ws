27b21ea53473c51a6660707b05fc09f867c77348
JackBorer <jackborer@utexas.edu>
2020-07-13 21:41:22 -0500

    Finally got it making sets+ raw ptr :(

--- src/source_localization/src/pf/pf.cpp
+++ src/source_localization/src/pf/pf.cpp
@@ -12,7 +12,7 @@ std::shared_ptr<pf_t> pf_alloc(int min_samples, int max_samples)
     // Are these the right type of pointers?
     std::shared_ptr<pf_t> pf;
     pf_sample_set_t* set;
-    std::unique_ptr<pf_sample_t> sample;
+    pf_sample_t* sample;
 
     pf.reset(new pf_t());
 
@@ -22,29 +22,28 @@ std::shared_ptr<pf_t> pf_alloc(int min_samples, int max_samples)
     pf->current_set = 0;
     for (j = 0; j < 2; j++)
     {
-      set = pf->sets + j;
-      
+      set = (pf->sets + j);
       set->sample_count = max_samples;
-      //set->samples = calloc(max_samples, sizeof(pf_sample_t));
+      // Dynamically assigned array containing all samples (particles)
+      set->samples = (pf_sample_t*) new pf_sample_t[max_samples];
 
-      // Start here to see how you can create an array with custom datatype
-      //https://stackoverflow.com/questions/8579694/how-do-i-declare-an-array-with-a-custom-class
-
-    //     for (i = 0; i < set->sample_count; i++)
-    //     {
-    //         sample = set->samples + i;
-    //         sample->pose.v[0] = 0.0;
-    //         sample->pose.v[1] = 0.0;
-    //         sample->pose.v[2] = 0.0;
-    //         sample->weight = 1.0 / max_samples;
-    //     }
+        for (i = 0; i < set->sample_count; i++)
+        {
+            sample = set->samples + i;
+            sample->state.v[0] = i;
+            sample->state.v[1] = 0.0;
+            sample->state.v[2] = 0.0;
+            sample->state.v[3] = 0.0;
+            sample->weight = 1.0 / max_samples;
+        }
 
       set->mean = pf_vector_zero();
       set->cov = pf_matrix_zero();
     }
 
-//   //set converged to 0
-//   pf_init_converged(pf);
+
+    // //set converged to 0
+    // pf_init_converged(pf);
 
   return pf;
 }
