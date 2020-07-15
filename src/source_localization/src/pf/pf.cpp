aa5510dc15d3cc4da774241e4c0d37e40fd38d40
JackBorer <jackborer@utexas.edu>
2020-07-15 13:23:02 -0500

    Uncomment block + Can's suggestion for refrence

--- src/source_localization/src/pf/pf.cpp
+++ src/source_localization/src/pf/pf.cpp
@@ -21,21 +21,22 @@ std::shared_ptr<pf_t> pf_alloc(int min_samples, int max_samples)
     // The particle filter being instantiated
     std::shared_ptr<pf_t> pf;
 
-    pf.reset(new pf_t());  //Allocate the memory for the pf
+    // Allocate and take ownership of the pf's memory
+    pf.reset(new pf_t());  
+
     pf->min_samples = min_samples;
     pf->max_samples = max_samples;
-
     pf->current_set = 0;
+
     for (j = 0; j < 2; j++)
     {
-      //set = pf->sets + j;
-  
+      // Allocate the two k and k+1 sets  
       pf->sets[j].sample_count = max_samples;
       pf->sets[j].samples = std::unique_ptr<pf_sample_t[]> (new pf_sample_t[max_samples]);  // Allocate the memory for the samples
 
       for (i = 0; i < pf->sets[j].sample_count; i++)
       {
-      //   sample = &set->samples[i]; // Iterate through the samples and fill out their values
+        // Allocate each sets sample
         pf->sets[j].samples[i].state.v[0] = i;
         pf->sets[j].samples[i].state.v[1] = 0.0;
         pf->sets[j].samples[i].state.v[2] = 0.0;
@@ -50,22 +51,22 @@ std::shared_ptr<pf_t> pf_alloc(int min_samples, int max_samples)
   return pf;
 }
 
-// void pf_init_uniform(std::shared_ptr<pf_t>  pf, std::shared_ptr<map_t> map,  double z_min, double z_max, double rate_min, double rate_max )
-// {
-//   // Future considerations: add tf ability to map if it had rotation 
-//   int i, j;
-//   std::unique_ptr<pf_sample_set_t>* set;
-//   pf_sample_t* sample;
+void pf_init_uniform(std::shared_ptr<pf_t>  pf, std::shared_ptr<map_t> map,  double z_min, double z_max, double rate_min, double rate_max )
+{
+  // Future considerations: add tf ability to map if it has had a rotation 
+  int i;
 
-//   double x_min= map->ros_map.info.origin.position.x - map->width/2.0 ; double x_max= map->ros_map.info.origin.position.x + map->width/2.0 ;
-//   double y_min= map->ros_map.info.origin.position.y - map->height/2.0 ; double y_max= map->ros_map.info.origin.position.y + map->height/2.0 ;
+  double x_min= map->ros_map.info.origin.position.x - map->width/2.0; 
+  double x_max= map->ros_map.info.origin.position.x + map->width/2.0;
+  double y_min= map->ros_map.info.origin.position.y - map->height/2.0; 
+  double y_max= map->ros_map.info.origin.position.y + map->height/2.0;
   
-//   set = &(pf->sets[0]);
-//   for (i = 0; i < (*set)->sample_count; i++)
-//   {
-//     // Iterate through the samples and fill out their values with uniform dist values within state space bounds
-//     sample= &((*set)->samples[i]); 
-//     sample->state= pf_pdf_uniform_sample(x_min, x_max, y_min, y_max, z_min, z_max, rate_min, rate_max);
-//   }
+  
+  for (i = 0; i < pf->sets[0].sample_count; i++)
+  {
+    // Iterate through the first set's samples and fill out their values with
+    // uniformly distributed values within state space bounds
+    pf->sets[0].samples[i].state= pf_pdf_uniform_sample(x_min, x_max, y_min, y_max, z_min, z_max, rate_min, rate_max);
+  }
 
-// }
+}
