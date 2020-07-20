233f89b75f00273eabf49773b5b5cff2a58439c1
JackBorer <jackborer@utexas.edu>
2020-07-20 15:49:27 -0500

    Custom data types and fncs

---
+++ src/source_localization/particle_filter/include/utils.h
@@ -0,0 +1,61 @@
+#ifndef UTILS_H
+#define UTIlS_H
+
+#include "vector"
+
+//3D position
+struct position
+{
+    double p[3];
+};
+
+
+// Each state represents a potential emission source in 3D (x,y,z) space emitting at a rate q
+struct state
+{
+    // Global 3D position and emission rate (x,y,z,q)
+    position pos;
+    double rate; 
+};
+
+struct particle
+{
+    state p;
+    double weight;
+};
+
+struct set
+{
+    int particle_count;
+    std::vector<particle> particles;
+};
+
+struct filter_state
+{
+    int current_set;
+    int counter;
+    set sets[2];
+};
+
+struct transformation
+{
+    double xyz_yaw[4];
+};
+
+struct state_space
+{
+    double x[2];
+    double y[2];
+    double z[2];
+    double rate[2];
+};
+
+
+
+
+//Return transform of test point into source local (source being the particle) frame
+position transform(const position& source, const position& test_point, const transformation& tf);
+
+
+
+#endif
