deefaaaf347af736935a78320e583f1813cf3f9f
JackBorer <jackborer@utexas.edu>
2020-07-21 11:13:25 -0500

    Filled out basic declarations

--- src/source_localization/particle_filter/include/predict.h
+++ src/source_localization/particle_filter/include/predict.h
@@ -9,12 +9,20 @@
 
 class Predict
 {
-    /*data*/
+//Classes are private by default :)
+    const filter_state& filter; //By being  a referencce is the filter autmatically updated?
+    wind_model wm;
+
+    std::vector<double> predicted_concentration;
+
 public:
     //If you want the function to use the pointee, pass a reference to it. 
     // There's no reason to tie the function to work only with some kind of smart pointer
-    Predict(wind_model wm, measurement meas, const filter_state& filter);
+    Predict(const filter_state& filt, wind_model wm );
     ~Predict();
+
+    void PredictConcentration(const measurement& meas, const position& test_point);
+    std::vector<double> GetPredictedConcentrations();
 };
 
 
--- src/source_localization/particle_filter/src/predict.cpp
+++ src/source_localization/particle_filter/src/predict.cpp
@@ -2,6 +2,21 @@
 
 
 #include "iostream"
-Predict::Predict(wind_model wm, measurement meas, const filter_state& filter){}
+Predict::Predict(const filter_state& filter, wind_model wm ):
+    filter{filter},
+    wm{wm}
+{
+    predicted_concentration.resize(filter.sets[0].particle_count);
+}
 
 Predict::~Predict(){}
+
+void Predict::PredictConcentration(const measurement& meas, const position& test_point)
+{
+    //Questions: is the tf fn. a potential inline candidate?
+    //For particle in set 0, 
+        //Convert the test point into a particle local frame using the azimuth and position data
+        //Calculate the concentration that we predict we should sense at our curerent location
+}
+
+std::vector<double> Predict::GetPredictedConcentrations() {return predicted_concentration;}
