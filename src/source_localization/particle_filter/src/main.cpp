e6380a6c0c32facfd2e7e3bf5015196556023e9f
JackBorer <jackborer@utexas.edu>
2020-07-21 11:13:46 -0500

    Clear

--- src/source_localization/particle_filter/src/main.cpp
+++ src/source_localization/particle_filter/src/main.cpp
@@ -6,23 +6,7 @@
 
 int main()
 {
-    state_space fake_ss;
-    fake_ss.x[0]=-10.0;
-    fake_ss.y[0]=-10.0;
-    fake_ss.z[0]=-10.0;
-    fake_ss.rate[0]=0.0;
-    fake_ss.x[1]=10.0;
-    fake_ss.y[1]=10.0;
-    fake_ss.z[1]=10.0;
-    fake_ss.rate[1]=1000.0;
-    int fake_particle_count = 400;
-    Initialize test(fake_particle_count, fake_ss );
-
-    std::unique_ptr<filter_state> fake_filter = test.GetFilter();
-
-    wind_model fake_wind_model;
-    measurement fake_measurement;
-    Predict fake_predict(fake_wind_model, fake_measurement, *fake_filter);
+    
 
 
 }
