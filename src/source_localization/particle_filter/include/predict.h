53b83c64a6a50911843312cf24e83f604b97e5c6
JackBorer <jackborer@utexas.edu>
2020-07-20 22:09:14 -0500

    First commit-smrt ptr pass by reference

--- src/source_localization/particle_filter/include/predict.h
+++ src/source_localization/particle_filter/include/predict.h
@@ -1,5 +1,22 @@
 #ifndef PREDICT_H
 #define PREDICT_H
 
+//stl
+#include "memory"
+
+//custom
+#include "utils.h"
+
+class Predict
+{
+    /*data*/
+public:
+    //If you want the function to use the pointee, pass a reference to it. 
+    // There's no reason to tie the function to work only with some kind of smart pointer
+    Predict(wind_model wm, measurement meas, const filter_state& filter);
+    ~Predict();
+};
+
+
 
 #endif
---
+++ src/source_localization/particle_filter/src/predict.cpp
@@ -0,0 +1,7 @@
+#include "predict.h"
+
+
+#include "iostream"
+Predict::Predict(wind_model wm, measurement meas, const filter_state& filter){}
+
+Predict::~Predict(){}
