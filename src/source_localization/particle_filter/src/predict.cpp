23e780d39d8bcfa1d17bcf1e3bce4a4ec5e4a6dd
JackBorer <jackborer@utexas.edu>
2020-07-22 16:14:41 -0500

    Built sigma and work on predictConc

--- src/source_localization/particle_filter/src/predict.cpp
+++ src/source_localization/particle_filter/src/predict.cpp
@@ -1,7 +1,10 @@
+#include "cmath"
+#include "iostream"
+
 #include "predict.h"
 
 
-#include "iostream"
+
 Predict::Predict(const filter_state& filter, wind_model wm ):
     filter{filter},
     wm{wm}
@@ -11,12 +14,28 @@ Predict::Predict(const filter_state& filter, wind_model wm ):
 
 Predict::~Predict(){}
 
-void Predict::PredictConcentration(const measurement& meas, const position& test_point)
+void Predict::predictConcentration(const measurement& meas, const position& test_point)
 {
     //Questions: is the tf fn. a potential inline candidate?
     //For particle in set 0, 
         //Convert the test point into a particle local frame using the azimuth and position data
         //Calculate the concentration that we predict we should sense at our curerent location
+    std::cout<<"test point "<< test_point.pos[0]<<" "<< test_point.pos[1]<<" "<< test_point.pos[2]<<"\n"; 
+    for (size_t i = 0; i < filter.sets[0].particle_count; i++)
+    {
+        position test_point_source_local= sl::transform(filter.sets[0].particles[i].p, test_point, meas.az);
+
+        std::cout<<"source position: "<< filter.sets[0].particles[i].p.s[0]<<" "<<filter.sets[0].particles[i].p.s[1]<<" "<<filter.sets[0].particles[i].p.s[2]<<"\n";
+        std::cout<<"test point source local: "<< test_point_source_local.pos[0]<<" "<<test_point_source_local.pos[1]<<" "<<test_point_source_local.pos[2]<<"\n";
+    }
+    
 }
 
-std::vector<double> Predict::GetPredictedConcentrations() {return predicted_concentration;}
+std::vector<double> Predict::getPredictedConcentrations() {return predicted_concentration;}
+
+
+double Predict::sigma(const double wm_i[3], const position& local_test_point)
+{
+    double x= local_test_point.pos[0]; //Source local downwind distance x
+    return wm_i[0]*x*std::pow(1+wm_i[1]*x,-wm_i[2]);
+}
