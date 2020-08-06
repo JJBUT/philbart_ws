d513278e1c5a1fe587eef6429478bc6a7a8e1c1f
JackBorer <jackborer@utexas.edu>
2020-08-06 15:00:37 -0500

    Reorganize file structure

--- src/nrg_gas_source_localization/include/nrg_sl_particle_filter.h
+++ src/nrg_gas_source_localization/include/nrg_sl_particle_filter.h
@@ -208,42 +208,4 @@ public:
     void printStatistics() const;
 };
 
-//utils
-namespace pf{
-    /**
-    * @brief Generate a vector of uniformly distributed random numbers from 0 to 1
-    * @param int How many independent random numbers you want 
-    * @return std::vector<double> A vector of length int holding unique uniformly distributed random numbers from 0 to 1 
-    */
-    std::vector<double> uniform_rn(int);
-    
-    /**
-    * @brief Generate a single uniformly distributed random number from 0 to 1
-    * @return double A single uniformly distributed random number from 0 to 1
-    */
-    double uniform_rn();
-    
-    /**
-    * @note The global origin is always referenced at ( 0,0,0 )
-    * @note The global zero azimuth is to the east (positive global x-axis)
-    * 
-    * @brief Transform a test point into the source local frame
-    * @param double* The source local test point which will be generated
-    * @param const double* The global test point, most often the location of a measurement
-    * @param const double* The global source point, most often the location of a particle
-    * @param const double& The rotation, most often the azimuth of the wind
-    * @return 
-    */
-    void transform(double*, const double*, const double*, const double&);
-    
-    /**
-    * @brief Generate a value from a gaussian distribution
-    * @param double The test point
-    * @param double The mean of the distribution
-    * @param double The standard deviation of the distribution
-    * @return double g(x) the value of the gaussian at the test point
-    */
-    double gaussian(double, double, double);
-} //END of pf namespace
-
 #endif
---
+++ src/nrg_gas_source_localization/src/nrg_sl_utils.cpp
@@ -0,0 +1,58 @@
+#include "nrg_sl_utils.h"
+
+#include "random"
+#include "algorithm" 
+
+namespace pf{
+std::vector<double> uniform_rn(int count){   
+    std::random_device rnd_device;
+    std::mt19937 mersenne_engine {rnd_device()};  // Generates random doubles
+    std::uniform_real_distribution<double> dist (0, 1.0);
+    auto gen = [&dist, &mersenne_engine](){return dist(mersenne_engine);};
+
+    std::vector<double> rnv(count);
+    std::generate(rnv.begin(), rnv.end(), gen);
+
+    return rnv;
+}
+
+double uniform_rn(){   
+    std::random_device rnd_device;
+    std::mt19937 mersenne_engine {rnd_device()};  // Generates random doubles
+    std::uniform_real_distribution<double> dist (0, 1.0);
+    
+    return dist(mersenne_engine);
+}
+
+void transform(double source_local_test_point[3], const double test_point[3], const double source[3], const double& rotation){
+    double temp_local[3];
+
+    // Translation
+    temp_local[0]= test_point[0] - source[0];
+    temp_local[1]= test_point[1] - source[1];
+    temp_local[2]= test_point[2] - source[2];
+
+    // Rotation
+    double R[3][3]= {};
+    R[0][0]= cos(rotation);
+    R[0][1]= -sin(rotation);
+    R[1][0]= sin(rotation);
+    R[1][1]= cos(rotation);
+    R[2][2]= 1;
+
+    // Recreate np.dot(local,R)
+    for (size_t i = 0; i < 3; i++){ // Traverse the vector
+        source_local_test_point[i]=0;
+        for (size_t j = 0; j < 3; j++){ //Traverse the columns of the matrix
+            source_local_test_point[i]+= temp_local[j] * R[j][i];
+        }
+    }
+
+    return;
+}
+
+double gaussian(double x, double mu, double sigma){
+    return (1/(std::sqrt(2*M_PI)*sigma))*std::exp(-0.5*std::pow((x-mu)/sigma, 2));
+}
+
+} //END of pf namespace
