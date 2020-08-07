aec1d43318f9473f13b15fca02af5de01601ee6e
JackBorer <jackborer@utexas.edu>
2020-08-07 11:26:30 -0500

    Testing dummy message_filter setup

--- src/nrg_gas_source_localization/src/nrg_sl_node.cpp
+++ src/nrg_gas_source_localization/src/nrg_sl_node.cpp
@@ -1,8 +1,27 @@
+#include <gmx200_anemometer/AnemometerMsg.h>
+
 #include <ros/ros.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+#include <boost/bind.hpp>
+
+using namespace gmx200_anemometer;
+
+void callback(const AnemometerMsg& msg_1, const AnemometerMsg& msg_2)
+{
+  // Solve all of perception here...
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "amcl");
   ros::NodeHandle nh;
+
+  message_filters::Subscriber<AnemometerMsg> sub_1(nh, "chanell_1", 1);
+  message_filters::Subscriber<AnemometerMsg> sub_2(nh, "channel_2", 1);
+  message_filters::TimeSynchronizer<AnemometerMsg, AnemometerMsg> sync(sub_1, sub_2, 10);
+  //sync.registerCallback(boost::bind(&callback, _1, _2));
   ros::spin();
   
   return(0);
