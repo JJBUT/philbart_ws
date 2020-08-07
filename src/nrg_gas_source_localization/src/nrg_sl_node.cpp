c47f763d24afbdf9b29bc683da2811fe22098b8d
JackBorer <jackborer@utexas.edu>
2020-08-07 15:10:29 -0500

    Msg_filter callback running

--- src/nrg_gas_source_localization/src/nrg_sl_node.cpp
+++ src/nrg_gas_source_localization/src/nrg_sl_node.cpp
@@ -1,12 +1,16 @@
 #include <gmx200_anemometer/AnemometerMsg.h>
 
 #include <ros/ros.h>
+#include <std_msgs/Int32.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
 #include <memory>
 #include <iostream>
+
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
 
 //https://answers.ros.org/question/280856/synchronizer-with-approximate-time-policy-in-a-class-c/
 using namespace gmx200_anemometer;
@@ -15,26 +19,26 @@ using namespace gmx200_anemometer;
 class Node
 {
 public:
-  Node():
-    p_nh{"~"}
+  Node()
   {
-    sub1.subscribe(nh, "in1", 10);
-    sub2.subscribe(nh, "in2", 10);
+    sub1.subscribe(nh, "/in1", 10);
+    sub2.subscribe(nh, "/in2", 10);
+    sync.reset(new Sync(MySyncPolicy(10), sub1, sub2));   
+    sync->registerCallback(boost::bind(&Node::callback, this, _1, _2));
   }
-  void callback(const AnemometerMsg &in1, const AnemometerMsg &in2)
-  {
-    std::cout<<"Msg one at time: "<<in1.header.stamp.sec<<" Msg two at time: "<<in2.header.stamp.sec<<std::endl;
-  } 
 
+  void callback(const AnemometerMsgConstPtr &in1, const AnemometerMsgConstPtr &in2)
+  {
+    std::cout<<"Msg one at time: "<<in1->header.stamp.sec<<" Msg two at time: "<<in2->header.stamp.sec<<std::endl;
+  } 
 
 private:
     ros::NodeHandle nh;
-    ros::NodeHandle p_nh;
     message_filters::Subscriber<AnemometerMsg> sub1;
     message_filters::Subscriber<AnemometerMsg> sub2;
     typedef message_filters::sync_policies::ApproximateTime<AnemometerMsg, AnemometerMsg> MySyncPolicy;
     typedef message_filters::Synchronizer<MySyncPolicy> Sync;
-    std::shared_ptr<Sync> sync;
+    boost::shared_ptr<Sync> sync;
 };
 
 
