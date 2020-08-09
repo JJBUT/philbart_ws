a41834126dbacae769a5da0c4b01e34d74158ea9
JackBorer <46971781+JackBorer@users.noreply.github.com>
2020-08-09 18:19:35 -0500

    Seven pillars of pretty code

--- src/nrg_gas_source_localization/src/nrg_gas_source_localization/nrg_sl_utils.cpp
+++ src/nrg_gas_source_localization/src/nrg_gas_source_localization/nrg_sl_utils.cpp
@@ -9,9 +9,14 @@
 namespace pf{
 std::vector<double> uniform_rn(int count){   
     std::random_device rnd_device;
-    std::mt19937 mersenne_engine {rnd_device()};  // Generates random doubles
+    std::mt19937 mersenne_engine {rnd_device()};  
     std::uniform_real_distribution<double> dist (0, 1.0);
-    auto gen = [&dist, &mersenne_engine](){return dist(mersenne_engine);};
+    
+    //TODO can I make this return double instead of auto for clarity
+    auto gen = [&dist, &mersenne_engine]()  
+    {
+        return dist(mersenne_engine);
+    };
 
     std::vector<double> rnv(count);
     std::generate(rnv.begin(), rnv.end(), gen);
@@ -31,17 +36,17 @@ void transform(double source_local_test_point[3], const double test_point[3], co
     double temp_local[3];
 
     // Translation
-    temp_local[0]= test_point[0] - source[0];
-    temp_local[1]= test_point[1] - source[1];
-    temp_local[2]= test_point[2] - source[2];
+    temp_local[0] = test_point[0] - source[0];
+    temp_local[1] = test_point[1] - source[1];
+    temp_local[2] = test_point[2] - source[2];
 
     // Rotation
-    double R[3][3]= {};
-    R[0][0]= cos(rotation);
-    R[0][1]= -sin(rotation);
-    R[1][0]= sin(rotation);
-    R[1][1]= cos(rotation);
-    R[2][2]= 1;
+    double R[3][3] = {};
+    R[0][0] = cos(rotation);
+    R[0][1] = -sin(rotation);
+    R[1][0] = sin(rotation);
+    R[1][1] = cos(rotation);
+    R[2][2] = 1.0;
 
     // Recreate np.dot(local,R)
     for (size_t i = 0; i < 3; i++){ // Traverse the vector
@@ -50,12 +55,11 @@ void transform(double source_local_test_point[3], const double test_point[3], co
             source_local_test_point[i]+= temp_local[j] * R[j][i];
         }
     }
-
     return;
 }
 
 double gaussian(double x, double mu, double sigma){
-    return (1/(std::sqrt(2*M_PI)*sigma))*std::exp(-0.5*std::pow((x-mu)/sigma, 2));
+    return ( 1/(std::sqrt(2*M_PI)*sigma) )*std::exp( -0.5*std::pow((x-mu)/sigma, 2) );
 }
 
 std::vector< std::vector<double> > read_data( std::string input_file_path ){
