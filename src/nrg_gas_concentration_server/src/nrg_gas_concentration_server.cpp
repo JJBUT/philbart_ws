9323c3560e93f95ddbd3f6b88d31ccbec7b23086
JackBorer <jackborer@utexas.edu>
2020-08-20 14:38:53 -0500

    Defined srv callbacks and build addSource

--- src/nrg_gas_concentration_server/src/nrg_gas_concentration_server.cpp
+++ src/nrg_gas_concentration_server/src/nrg_gas_concentration_server.cpp
@@ -1,5 +1,7 @@
 #include <nrg_gas_concentration_server/nrg_gas_concentration_server.h>
 
+#include <ros/console.h>
+
 namespace nrg_gas_concentration_server
 {
     SimulatedSourceServer::SimulatedSourceServer()
@@ -7,15 +9,57 @@ namespace nrg_gas_concentration_server
     {
         set_gas_source_srv_ = private_nh_.advertiseService( "set_source", 
                                                              &SimulatedSourceServer::addSource, 
-                                                             this);
+                                                             this );
 
         get_concentration_srv_ = private_nh_.advertiseService( "get_concentration", 
-                                                       &SimulatedSourceServer::getConcentration, 
-                                                       this);
+                                                               &SimulatedSourceServer::getConcentration, 
+                                                               this );
                                                        
         clear_sources_srv_ = private_nh_.advertiseService( "clear_sources", 
-                                                   &SimulatedSourceServer::clearSources, 
-                                                   this);
+                                                           &SimulatedSourceServer::clearSources, 
+                                                           this );
 
     }
+
+    bool SimulatedSourceServer::addSource( SetSource::Request &req, 
+                                           std_srvs::Empty::Response &res ) 
+    {
+      const std::lock_guard<std::mutex> lock( sources_mutex_ );
+      if ( req.source.rate <= 0 ) 
+      {
+        ROS_DEBUG_STREAM_NAMED( "SimulatedSourceServer::addSource()", "Source rate must be positive" );
+        return 0;
+      }
+
+      sources_.push_back( req.source );
+      //TODO-add visualization component
+
+      return 1;
+    }
+
+    bool SimulatedSourceServer::getConcentration( GetConcentration::Request &req, 
+                                                  GetConcentration::Response &res ) 
+    {
+      const std::lock_guard<std::mutex> lock(sources_mutex_);
+      return 0;
+    }
+
+    bool SimulatedSourceServer::clearSources( std_srvs::Empty::Request &req, 
+                                              std_srvs::Empty::Response &res ) 
+    {
+      const std::lock_guard<std::mutex> lock(sources_mutex_);
+      return 0;
+    }
 };
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "simulated_gas_source_server");
+
+  nrg_gas_concentration_server::SimulatedSourceServer node;
+  
+  ros::spin();
+
+  return 0;
+}
